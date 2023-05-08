c00a48218e3d20c4f1476cadc5b190dc28946577
folorunso21 <folorunsoolamilekan2006@gmail.com>
2023-05-08 23:33:25 +0100

    firstcommit

---
+++ main.js
@@ -0,0 +1,8 @@
+<div id="container">
+        <input type="text" id="Boy" placeholder="Male">
+    <input type="text" name="" id="Girl" placeholder="Female">
+    <button onclick="result()">Generate love</button>
+    <!-- <input type="text" id="Result" placeholder="result"> -->
+    <p id="inp"></p>
+    </div>
+    
